fd7a4d98b780d41d4c930daa093f8506ac12eb96
xiaojie <xiaojie@gmail.com>
2024-08-14 21:08:12 +0800

    update

--- src/main/java/com/ruoyi/system/domain/LotteryRelation.java
+++ src/main/java/com/ruoyi/system/domain/LotteryRelation.java
@@ -1,10 +1,11 @@
 package com.ruoyi.system.domain;
 
-import io.swagger.annotations.ApiModelProperty;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import com.ruoyi.common.annotation.Excel;
 import com.ruoyi.common.core.domain.BaseEntity;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 
 /**
  * 码表对象 t_lottery_relation
@@ -12,103 +13,52 @@ import com.ruoyi.common.core.domain.BaseEntity;
  * @author ruoyi
  * @date 2024-08-14
  */
-public class LotteryRelation extends BaseEntity
-{
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class LotteryRelation extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
-    /** id */
+    /**
+     * id
+     */
     @ApiModelProperty(example = "id")
     private Long id;
 
-    /** 数字 */
+    /**
+     * 数字
+     */
     @Excel(name = "数字")
     @ApiModelProperty(example = "数字")
     private String number;
 
-    /** 颜色(1红色，2-蓝色，3-绿色) */
+    /**
+     * 颜色(1红色，2-蓝色，3-绿色)
+     */
     @Excel(name = "颜色(1红色，2-蓝色，3-绿色)")
     @ApiModelProperty(example = "颜色(1红色，2-蓝色，3-绿色)")
-    private Integer color;
+    private Byte color;
 
-    /** 五行(金木水火土) */
+    /**
+     * 五行(金木水火土)
+     */
     @Excel(name = "五行(金木水火土)")
     @ApiModelProperty(example = "五行")
     private String wx;
 
-    /** 生肖(鼠牛虎兔...) */
+    /**
+     * 生肖(鼠牛虎兔...)
+     */
     @Excel(name = "生肖(鼠牛虎兔...)")
     @ApiModelProperty(example = "生肖")
     private String sx;
 
-    /** 年份 */
+    /**
+     * 年份
+     */
     @Excel(name = "年份")
     @ApiModelProperty(example = "年份")
     private Integer year;
 
-    public void setId(Long id)
-    {
-        this.id = id;
-    }
-
-    public Long getId()
-    {
-        return id;
-    }
-    public void setNumber(String number)
-    {
-        this.number = number;
-    }
-
-    public String getNumber()
-    {
-        return number;
-    }
-    public void setColor(Integer color)
-    {
-        this.color = color;
-    }
-
-    public Integer getColor()
-    {
-        return color;
-    }
-    public void setWx(String wx)
-    {
-        this.wx = wx;
-    }
-
-    public String getWx()
-    {
-        return wx;
-    }
-    public void setSx(String sx)
-    {
-        this.sx = sx;
-    }
-
-    public String getSx()
-    {
-        return sx;
-    }
-    public void setYear(Integer year)
-    {
-        this.year = year;
-    }
-
-    public Integer getYear()
-    {
-        return year;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("id", getId())
-            .append("number", getNumber())
-            .append("color", getColor())
-            .append("wx", getWx())
-            .append("sx", getSx())
-            .append("year", getYear())
-            .toString();
-    }
 }
