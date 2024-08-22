0f58eb3c08e966c50120a9ea4ff443e12d8e8431
xiaojie <xiaojie@gmail.com>
2024-08-22 20:46:08 +0800

    update

--- src/main/java/com/ruoyi/system/domain/BetLimit.java
+++ src/main/java/com/ruoyi/system/domain/BetLimit.java
@@ -1,110 +1,53 @@
 package com.ruoyi.system.domain;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import com.ruoyi.common.annotation.Excel;
 import com.ruoyi.common.core.domain.BaseEntity;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 
 /**
  * 投注限制对象 t_bet_limit
- * 
+ *
  * @author ruoyi
  * @date 2024-08-22
  */
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
 public class BetLimit extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
 
     /**  */
+    @ApiModelProperty(example = "id")
     private Long id;
 
     /** code */
     @Excel(name = "code")
+    @ApiModelProperty(example = "code")
     private String code;
 
     /** 名称 */
     @Excel(name = "名称")
+    @ApiModelProperty(example = "名称")
     private String name;
 
     /** 最小投注结果 */
     @Excel(name = "最小投注结果")
-    private Long minBetCount;
+    @ApiModelProperty(example = "最小投注结果")
+    private Integer minBetCount;
 
     /** 最大投注结果 */
     @Excel(name = "最大投注结果")
-    private Long maxBetCount;
+    @ApiModelProperty(example = "最大投注结果")
+    private Integer maxBetCount;
 
     /** 状态，1-有效，0-无效 */
     @Excel(name = "状态，1-有效，0-无效")
-    private Integer status;
+    @ApiModelProperty(example = "状态，1-有效，0-无效")
+    private Byte status;
 
-    public void setId(Long id) 
-    {
-        this.id = id;
-    }
-
-    public Long getId() 
-    {
-        return id;
-    }
-    public void setCode(String code) 
-    {
-        this.code = code;
-    }
-
-    public String getCode() 
-    {
-        return code;
-    }
-    public void setName(String name) 
-    {
-        this.name = name;
-    }
-
-    public String getName() 
-    {
-        return name;
-    }
-    public void setMinBetCount(Long minBetCount) 
-    {
-        this.minBetCount = minBetCount;
-    }
-
-    public Long getMinBetCount() 
-    {
-        return minBetCount;
-    }
-    public void setMaxBetCount(Long maxBetCount) 
-    {
-        this.maxBetCount = maxBetCount;
-    }
-
-    public Long getMaxBetCount() 
-    {
-        return maxBetCount;
-    }
-    public void setStatus(Integer status) 
-    {
-        this.status = status;
-    }
-
-    public Integer getStatus() 
-    {
-        return status;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("id", getId())
-            .append("code", getCode())
-            .append("name", getName())
-            .append("minBetCount", getMinBetCount())
-            .append("maxBetCount", getMaxBetCount())
-            .append("status", getStatus())
-            .append("remark", getRemark())
-            .append("createTime", getCreateTime())
-            .append("updateTime", getUpdateTime())
-            .toString();
-    }
 }
--- src/main/java/com/ruoyi/system/domain/UserBetLimitAmount.java
+++ src/main/java/com/ruoyi/system/domain/UserBetLimitAmount.java
@@ -1,6 +1,11 @@
 package com.ruoyi.system.domain;
 
 import java.math.BigDecimal;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import com.ruoyi.common.annotation.Excel;
@@ -8,118 +13,51 @@ import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
  * 用户投注限额对象 t_user_bet_limit_amount
- * 
+ *
  * @author ruoyi
  * @date 2024-08-22
  */
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
 public class UserBetLimitAmount extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
 
     /** ID */
+    @ApiModelProperty(example = "id")
     private String id;
 
     /** 用户ID */
     @Excel(name = "用户ID")
+    @ApiModelProperty(example = "用户ID")
     private Long userId;
 
     /** 彩种ID */
     @Excel(name = "彩种ID")
+    @ApiModelProperty(example = "彩种ID")
     private Long lotteryId;
 
     /** 限额类型 */
     @Excel(name = "限额类型")
-    private Integer type;
+    @ApiModelProperty(example = "限额类型")
+    private Byte type;
 
     /** 单注下限 */
     @Excel(name = "单注下限")
+    @ApiModelProperty(example = "单注下限")
     private BigDecimal singleTimeBetMinAmount;
 
     /** 单注上限 */
     @Excel(name = "单注上限")
+    @ApiModelProperty(example = "单注上限")
     private BigDecimal singleTimeBetMaxAmount;
 
     /** 单期限额 */
     @Excel(name = "单期限额")
+    @ApiModelProperty(example = "单期限额")
     private BigDecimal singlePeriodBetMaxAmount;
 
-    public void setId(String id) 
-    {
-        this.id = id;
-    }
 
-    public String getId() 
-    {
-        return id;
-    }
-    public void setUserId(Long userId) 
-    {
-        this.userId = userId;
-    }
-
-    public Long getUserId() 
-    {
-        return userId;
-    }
-    public void setLotteryId(Long lotteryId) 
-    {
-        this.lotteryId = lotteryId;
-    }
-
-    public Long getLotteryId() 
-    {
-        return lotteryId;
-    }
-    public void setType(Integer type) 
-    {
-        this.type = type;
-    }
-
-    public Integer getType() 
-    {
-        return type;
-    }
-    public void setSingleTimeBetMinAmount(BigDecimal singleTimeBetMinAmount) 
-    {
-        this.singleTimeBetMinAmount = singleTimeBetMinAmount;
-    }
-
-    public BigDecimal getSingleTimeBetMinAmount() 
-    {
-        return singleTimeBetMinAmount;
-    }
-    public void setSingleTimeBetMaxAmount(BigDecimal singleTimeBetMaxAmount) 
-    {
-        this.singleTimeBetMaxAmount = singleTimeBetMaxAmount;
-    }
-
-    public BigDecimal getSingleTimeBetMaxAmount() 
-    {
-        return singleTimeBetMaxAmount;
-    }
-    public void setSinglePeriodBetMaxAmount(BigDecimal singlePeriodBetMaxAmount) 
-    {
-        this.singlePeriodBetMaxAmount = singlePeriodBetMaxAmount;
-    }
-
-    public BigDecimal getSinglePeriodBetMaxAmount() 
-    {
-        return singlePeriodBetMaxAmount;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("id", getId())
-            .append("userId", getUserId())
-            .append("lotteryId", getLotteryId())
-            .append("type", getType())
-            .append("singleTimeBetMinAmount", getSingleTimeBetMinAmount())
-            .append("singleTimeBetMaxAmount", getSingleTimeBetMaxAmount())
-            .append("singlePeriodBetMaxAmount", getSinglePeriodBetMaxAmount())
-            .append("remark", getRemark())
-            .append("createTime", getCreateTime())
-            .append("updateTime", getUpdateTime())
-            .toString();
-    }
 }
