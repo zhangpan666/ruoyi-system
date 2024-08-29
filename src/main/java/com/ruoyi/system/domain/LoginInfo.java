8b0d513faa0796937aae4c624963de8cdb1a36f2
xiaojie <xiaojie@gmail.com>
2024-08-29 22:20:39 +0800

    update

--- src/main/java/com/ruoyi/system/domain/LoginInfo.java
+++ src/main/java/com/ruoyi/system/domain/LoginInfo.java
@@ -2,6 +2,7 @@ package com.ruoyi.system.domain;
 
 import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -25,43 +26,53 @@ public class LoginInfo extends BaseEntity
     private static final long serialVersionUID = 1L;
 
     /** 主键ID */
+    @ApiModelProperty(example = "id")
     private Long id;
 
     /** 用户ID */
     @Excel(name = "用户ID")
+    @ApiModelProperty(example = "用户ID")
     private Long userId;
 
     /** 平台id */
     @Excel(name = "平台id")
+    @ApiModelProperty(example = "平台ID")
     private Long platformId;
 
     /** 平台用户id */
     @Excel(name = "平台用户id")
+    @ApiModelProperty(example = "id")
     private Long platformUserId;
 
     /** 客户端类型 */
     @Excel(name = "客户端类型")
-    private Integer clientType;
+    @ApiModelProperty(example = "id")
+    private Byte clientType;
 
     /** 登录ip */
     @Excel(name = "登录ip")
+    @ApiModelProperty(example = "id")
     private String ip;
 
     /** 设备ID */
     @Excel(name = "设备ID")
+    @ApiModelProperty(example = "id")
     private String deviceId;
 
     /** token */
     @Excel(name = "token")
+    @ApiModelProperty(example = "id")
     private String token;
 
     /** 状态 */
     @Excel(name = "状态")
+    @ApiModelProperty(example = "id")
     private Byte status;
 
     /** 过期时间 */
     @JsonFormat(pattern = "yyyy-MM-dd")
-    @Excel(name = "过期时间", width = 30, dateFormat = "yyyy-MM-dd")
+    @Excel(name = "过期时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(example = "id")
     private Date endTime;
 
 }
