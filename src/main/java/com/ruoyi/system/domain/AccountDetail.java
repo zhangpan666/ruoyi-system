aae57ca1ad86636d448a37c4da7a23d8a39b757e
xiaojie <xiaojie@gmail.com>
2024-11-07 22:45:34 +0800

    update

--- src/main/java/com/ruoyi/system/domain/AccountDetail.java
+++ src/main/java/com/ruoyi/system/domain/AccountDetail.java
@@ -29,68 +29,69 @@ public class AccountDetail
 
     /** 主键ID */
     @Excel(name = "ID")
-    @ApiModelProperty(example = "id")
+    @ApiModelProperty(value = "ID")
     private Long id;
 
     /** 用户ID */
     @Excel(name = "用户ID")
-    @ApiModelProperty(example = "用户ID")
+    @ApiModelProperty(value = "用户ID")
     private Long userId;
 
     /** 关联ID */
     @Excel(name = "关联ID")
-    @ApiModelProperty(example = "关联ID")
+    @ApiModelProperty(value = "关联ID")
     private Long relatedId;
 
     /** 订单号 */
     @Excel(name = "订单号")
-    @ApiModelProperty(example = "订单号")
+    @ApiModelProperty(value = "订单号")
     private String orderNo;
 
     /** 商户订单号 */
     @Excel(name = "商户订单号")
-    @ApiModelProperty(example = "商户订单号")
+    @ApiModelProperty(value = "商户订单号")
     private String mchOrderNo;
 
     /** 收支类型(-1：支出，1：收入，0：非收支记录) */
-    @ApiModelProperty(example = "收支类型(-1：支出，1：收入，0：非收支记录)")
-    @Excel(name = "收支类型", readConverterExp = "-1-支出,1-收入,0-非收支记录")
+    @ApiModelProperty(value = "收支类型(-1：支出，1：收入，0：非收支记录)")
+    @Excel(name = "收支类型", readConverterExp = "-1=支出,1=收入,0=非收支记录")
     private Byte paymentType;
 
     /** 金额 */
     @Excel(name = "金额")
-    @ApiModelProperty(example = "金额")
+    @ApiModelProperty(value = "金额")
     private BigDecimal amount;
 
     /** 当前余额 */
     @Excel(name = "当前余额")
-    @ApiModelProperty(example = "当前余额")
+    @ApiModelProperty(value = "当前余额")
     private BigDecimal availableBalance;
 
     /** 0-退款，1-额度转入，2-额度转出，3-投注，4-派彩，5-冻结 */
-    @Excel(name = "状态", readConverterExp = "0-退款,1-额度转入,2-额度转出,3-投注，4-派彩,5-冻结,6-解冻,7-撤单")
-    @ApiModelProperty(example = "id")
+    @Excel(name = "交易类型", readConverterExp = "0=退款,1=额度转入,2=额度转出,3=投注，4=派彩,5=冻结,6=解冻,7=撤单")
+    @ApiModelProperty(value = "id")
     private Byte tradeType;
 
     /** 状态(1-到账，2-冻结,3-取消) */
-    @Excel(name = "状态(1-到账，2-冻结,3-取消)")
-    @ApiModelProperty(example = "状态(1-到账，2-冻结,3-取消)")
+    @Excel(name = "状态", readConverterExp = "1=到账,2=冻结,3=取消")
+    @ApiModelProperty(value = "状态(1-到账，2-冻结,3-取消)")
     private Byte status;
 
     /** 创建时间 */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Excel(name = "创建时间", width = 57, dateFormat = "yyyy-MM-dd HH:mm:ss")
-    @ApiModelProperty(example = "创建时间")
+    @ApiModelProperty(value = "创建时间")
     private Date createTime;
 
     /** 更新时间 */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Excel(name = "更新时间", width = 57, dateFormat = "yyyy-MM-dd HH:mm:ss")
-    @ApiModelProperty(example = "更新时间")
+    @ApiModelProperty(value = "更新时间")
     private Date updateTime;
 
     /** 备注 */
-    @ApiModelProperty(example = "备注")
+    @Excel(name = "备注")
+    @ApiModelProperty(value = "备注")
     private String remark;
 
     private Date beginTime;
