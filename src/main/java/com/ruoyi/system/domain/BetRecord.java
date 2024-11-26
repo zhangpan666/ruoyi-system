457bd2c4ef2670b42a98c979ee4654b26f7f1b2a
xiaojie <xiaojie@gmail.com>
2024-11-26 23:52:07 +0800

    update

--- src/main/java/com/ruoyi/system/domain/BetRecord.java
+++ src/main/java/com/ruoyi/system/domain/BetRecord.java
@@ -38,51 +38,10 @@ public class BetRecord extends BaseEntity
     @ApiModelProperty(value = "ID，字符串类型")
     private String idStr;
 
-    /** 用户ID */
-    @Excel(name = "用户ID")
-    @ApiModelProperty(value = "用户ID")
-    private Long userId;
-
-    /** 投注金额 */
-    @Excel(name = "投注金额")
-    @ApiModelProperty(value = "投注金额")
-    private BigDecimal betAmount;
-
-    /** 有效投注金额 */
-    @Excel(name = "有效投注金额")
-    @ApiModelProperty(value = "有效投注金额")
-    private BigDecimal validBetAmount;
-
-
-    /** 可赢金额 */
-    @Excel(name = "可赢金额")
-    @ApiModelProperty(value = "可赢金额")
-    private BigDecimal canWinAmount;
-
-    /** 下注内容 */
-//    @Excel(name = "下注内容(展示)")
-    @ApiModelProperty(value = "下注内容(展示)")
-    private String betContent;
-
-    /** 下注内容 */
-    @Excel(name = "下注内容")
-    @ApiModelProperty(value = "下注内容")
-    private String displayBetContent;
-
-    /** 赔率 */
-    @Excel(name = "赔率")
-    @ApiModelProperty(value = "赔率")
-    private BigDecimal betOdds;
-
-    /** 附加赔率 */
-    @Excel(name = "附加赔率")
-    @ApiModelProperty(value = "附加赔率")
-    private BigDecimal betAdditionalOdds;
-
-    /** 平台id */
-    @Excel(name = "平台ID")
-    @ApiModelProperty(value = "平台ID")
-    private Long platformId;
+    /** 注单编号 */
+    @Excel(name = "注单编号")
+    @ApiModelProperty(value = "注单编号")
+    private String betNo;
 
     /**
      * 平台名称
@@ -96,10 +55,10 @@ public class BetRecord extends BaseEntity
     @ApiModelProperty(value = "平台用户ID")
     private Long platformUserId;
 
-    /** 彩种ID */
-    @Excel(name = "彩种ID")
-    @ApiModelProperty(value = "彩种ID")
-    private Long lotteryId;
+    /** 用户ID */
+    @Excel(name = "用户ID")
+    @ApiModelProperty(value = "用户ID")
+    private Long userId;
 
     /**
      * 彩种名称
@@ -118,6 +77,52 @@ public class BetRecord extends BaseEntity
     @ApiModelProperty(value = "玩法名称")
     private String playTypeName;
 
+    /** 下注内容 */
+//    @Excel(name = "下注内容(展示)")
+    @ApiModelProperty(value = "下注内容(展示)")
+    private String betContent;
+
+    /** 下注内容 */
+    @Excel(name = "下注内容")
+    @ApiModelProperty(value = "下注内容")
+    private String displayBetContent;
+
+    /** 投注金额 */
+    @Excel(name = "投注金额")
+    @ApiModelProperty(value = "投注金额")
+    private BigDecimal betAmount;
+
+    /** 有效投注金额 */
+    @Excel(name = "有效投注金额")
+    @ApiModelProperty(value = "有效投注金额")
+    private BigDecimal validBetAmount;
+
+    /** 可赢金额 */
+    @Excel(name = "可赢金额")
+    @ApiModelProperty(value = "可赢金额")
+    private BigDecimal canWinAmount;
+
+    @Excel(name = "注数")
+    @ApiModelProperty(value = "注数")
+    private Integer singleTimeCount;
+
+    /** 赔率 */
+    @Excel(name = "赔率")
+    @ApiModelProperty(value = "赔率")
+    private BigDecimal betOdds;
+
+    /** 附加赔率 */
+    @Excel(name = "附加赔率")
+    @ApiModelProperty(value = "附加赔率")
+    private BigDecimal betAdditionalOdds;
+
+    /** 下注时间 */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Excel(name = "下注时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "下注时间")
+    @JSONField(format = DateUtils.DATE_TIME_FORMAT)
+    private Date betTime;
+
     /** 输赢状态，1-赢，0-输，2-打和 */
     @Excel(name = "输赢状态", readConverterExp = "1=赢,0=输,2=打和")
     @ApiModelProperty(value = "输赢状态，1-赢，0-输，2-打和")
@@ -133,37 +138,10 @@ public class BetRecord extends BaseEntity
     @ApiModelProperty(value = "输赢金额")
     private BigDecimal winLoseAmount;
 
-    /** 玩法code */
-    @Excel(name = "玩法code")
-    @ApiModelProperty(value = "玩法code")
-    private Byte playTypeCode;
-
-    /** 玩法明细code */
-    @Excel(name = "玩法明细code")
-    @ApiModelProperty(value = "玩法明细code")
-    private String playTypeDetailCode;
-
-    /** 注单编号 */
-    @Excel(name = "注单编号")
-    @ApiModelProperty(value = "注单编号")
-    private String betNo;
-
     /** 注单状态，0-待结算，1-结算成功，2-结算失败，3-已撤单 */
     @Excel(name = "注单状态", readConverterExp = "0=待结算,1=结算成功,2=结算失败,3=已撤单")
     @ApiModelProperty(value = "注单状态，0-待结算，1-结算成功，2-结算失败，3-已撤单")
     private Byte status;
-
-    /** 撤单类型，1-个人撤单，2-系统撤单，3-系统管理员撤单 */
-    @Excel(name = "撤单类型", readConverterExp = "1=个人撤单,2=系统撤单,3=管理员撤单")
-    @ApiModelProperty(value = "撤单类型，1-个人撤单，2-系统撤单，3-系统管理员撤单")
-    private Byte cancelType;
-
-    /** 下注时间 */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Excel(name = "下注时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
-    @ApiModelProperty(value = "下注时间")
-    @JSONField(format = DateUtils.DATE_TIME_FORMAT)
-    private Date betTime;
 
     /** 结算时间 */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
@@ -172,14 +150,34 @@ public class BetRecord extends BaseEntity
     @JSONField(format = DateUtils.DATE_TIME_FORMAT)
     private Date countTime;
 
-    @Excel(name = "单注投注金额")
+    /** 撤单类型，1-个人撤单，2-系统撤单，3-系统管理员撤单 */
+    @Excel(name = "撤单类型", readConverterExp = "1=个人撤单,2=系统撤单,3=管理员撤单")
+    @ApiModelProperty(value = "撤单类型，1-个人撤单，2-系统撤单，3-系统管理员撤单")
+    private Byte cancelType;
+
+    /** 平台id */
+//    @Excel(name = "平台ID")
+    @ApiModelProperty(value = "平台ID")
+    private Long platformId;
+
+    /** 彩种ID */
+//    @Excel(name = "彩种ID")
+    @ApiModelProperty(value = "彩种ID")
+    private Long lotteryId;
+
+    /** 玩法code */
+//    @Excel(name = "玩法code")
+    @ApiModelProperty(value = "玩法code")
+    private Byte playTypeCode;
+
+    /** 玩法明细code */
+//    @Excel(name = "玩法明细code")
+    @ApiModelProperty(value = "玩法明细code")
+    private String playTypeDetailCode;
+
+//    @Excel(name = "单注投注金额")
     @ApiModelProperty(value = "单注投注金额")
     private BigDecimal singleTimeBetAmount;
-
-    @Excel(name = "注数")
-    @ApiModelProperty(value = "注数")
-    private Integer singleTimeCount;
-
 
     private Byte timeType;
 
