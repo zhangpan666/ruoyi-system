6d9b4e5458e86c921e9c109897e1e8826467b385
xiaojie <xiaojie@gmail.com>
2026-01-30 21:28:19 +0800

    update

--- src/main/java/com/ruoyi/system/pojo/LotteryPrizeStatVO.java
+++ src/main/java/com/ruoyi/system/pojo/LotteryPrizeStatVO.java
@@ -22,18 +22,6 @@ public class LotteryPrizeStatVO implements Serializable {
     @Excel(name = "日期", width = 30, dateFormat = "yyyy-MM-dd")
     private Date statDate;
 
-    @ApiModelProperty(value = "用户ID")
-    @Excel(name = "用户ID")
-    private Long userId;
-
-    @ApiModelProperty(value = "彩种ID")
-    @Excel(name = "彩种ID")
-    private Long lotteryId;
-
-    @ApiModelProperty(value = "彩种名称")
-    @Excel(name = "彩种名称")
-    private String lotteryName;
-
     @ApiModelProperty(value = "总爆奖次数")
     @Excel(name = "总爆奖次数")
     private int totalPrizeCount;
