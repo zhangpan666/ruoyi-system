84be91569201f9712bfb0ad99e9f88b8448c52f8
xiaojie <xiaojie@gmail.com>
2026-02-02 18:29:44 +0800

    update

--- src/main/java/com/ruoyi/system/pojo/LotteryPrizeStatDTO.java
+++ src/main/java/com/ruoyi/system/pojo/LotteryPrizeStatDTO.java
@@ -1,10 +1,6 @@
 package com.ruoyi.system.pojo;
 
 
-import com.alibaba.fastjson.annotation.JSONField;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.light.core.utils.DateUtils;
-import com.ruoyi.common.annotation.Excel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -15,30 +11,24 @@ import java.util.Date;
 
 @Data
 @Accessors(chain = true)
-public class LotteryPrizeStatVO implements Serializable {
+public class LotteryPrizeStatDTO implements Serializable {
 
     @ApiModelProperty(value = "彩种ID")
     private Long lotteryId;
 
-    @Excel(name = "彩种名称")
     @ApiModelProperty(value = "彩种名称")
     private String lotteryName;
 
     @ApiModelProperty(value = "日期")
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @Excel(name = "日期", width = 30, dateFormat = "yyyy-MM-dd")
     private Date statDate;
 
     @ApiModelProperty(value = "总爆奖次数")
-    @Excel(name = "总爆奖次数")
     private int totalPrizeCount;
 
     @ApiModelProperty(value = "总爆奖人次")
-    @Excel(name = "总爆奖人次")
     private int totalPrizePeopleCount;
 
     @ApiModelProperty(value = "总爆奖金额")
-    @Excel(name = "总爆奖金额")
     private BigDecimal totalPrizeAmount;
 
 }
